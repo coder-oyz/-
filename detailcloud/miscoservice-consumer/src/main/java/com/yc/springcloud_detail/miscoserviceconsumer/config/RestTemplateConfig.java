742774c4be572046e2a35c2c1dab19b8bbdea6f9
coder-oyz <1583807155@qq.com>
2020-10-13 18:37:17 +0800

    detailcloud加入security和eureka

--- detailcloud/miscoservice-consumer/src/main/java/com/yc/springcloud_detail/miscoserviceconsumer/config/RestTemplateConfig.java
+++ detailcloud/miscoservice-consumer/src/main/java/com/yc/springcloud_detail/miscoserviceconsumer/config/RestTemplateConfig.java
@@ -1,7 +1,6 @@
 package com.yc.springcloud_detail.miscoserviceconsumer.config;
 
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
